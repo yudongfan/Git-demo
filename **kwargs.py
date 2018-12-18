745b91a961df3c9d5ce0867a80ae93289af0de28
yudongfan <45969068+yudongfan@users.noreply.github.com>
2018-12-19 00:08:04 +1100

    Update **kwargs.py

--- **kwargs.py
+++ **kwargs.py
@@ -6,17 +6,4 @@ def m1(*args,**kwargs):
 m1()
 
 dic_inty={'name':'inty','age':'18 years od','height':'180cm','weight':'193b'}
-def someone(dic_someone):
-
-    for k,v in dic_someone.items():
-         print(k,':',v)
-
-someone(dic_inty)
-
-
-def someone(**kwargs):
-    for k,v in kwargs.items():
-         print(k,':',v)
-someone(name='xiao',age='18',height='192')
-
-#aaaa
+阿斯顿发
